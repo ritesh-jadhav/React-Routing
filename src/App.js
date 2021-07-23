7c22d6132082b1c4ec93b6258b0c15c8558e0e1e
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-07-24 01:57:12 +0530

    Registering routes functionallity completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./Pages/AllQuotes";
+import NewQuote from "./Pages/NewQuote";
+import QuoteDetail from "./Pages/QuoteDetail";
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes/>
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail/>
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote/>
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/Pages/QuoteDetail.js
@@ -0,0 +1,4 @@
+const QuoteDetail = () => {
+    return <h1> Quotes details Page</h1>
+};
+export default QuoteDetail;
