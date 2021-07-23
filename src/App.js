cd21235f58224808e31425814bd106459154fd9d
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-07-24 02:36:39 +0530

    adding wrapper component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import AllQuotes from "./Pages/AllQuotes";
 import NewQuote from "./Pages/NewQuote";
 import QuoteDetail from "./Pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
 
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
