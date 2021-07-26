bec0cb12f1fc7f0cd473ceea0df18f115c9b8e72
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-07-26 23:48:54 +0530

    navigate via useHistory in onAddQuoteHandler

--- src/Pages/NewQuote.js
+++ src/Pages/NewQuote.js
@@ -1,9 +1,14 @@
-import QuoteForm from '../components/quotes/QuoteForm';
+import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
+
 const NewQuote = () => {
-    const addQuoteHandler =( quoteData) =>{
-        console.log(quoteData)
-    };  
-  return <QuoteForm onAddQuote={addQuoteHandler}/>;
+    const history = useHistory();
+    const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+   
+    history.push("/quotes");
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
